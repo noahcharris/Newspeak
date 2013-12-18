28aaab56ff1c38259d87759407eec7f0c63c384d
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-18 15:33:57 -0800

    Added main.js

---
+++ client/main.js
@@ -0,0 +1,12 @@
+
+
+
+var width = window.innerWidth * 0.75;
+var height = window.innerHeight * 0.75;
+
+
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height);
+
+console.log('main.js loaded');
