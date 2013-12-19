a4d2ecb8109fa3f34f2184345e7b140b5b1b619a
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-19 12:59:51 -0800

    testing

--- server/request-handler.js
+++ server/request-handler.js
@@ -16,9 +16,10 @@ module.exports = function (request, response) {
   console.log('serving ' + request.method + ' request at ' + request.url);
   response.writeHead(200, {'Content-type':'text/plain'});
 
-  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
+  // connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  // if (err) throw err;
 
-  response.end('The solution is: ' + rows[0].solution);
+  // response.end('The solution is: ' + rows[0].solution);
+  response.end("hhihihihi");
 });
 }
