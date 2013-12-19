2af845834c4bd39944f46dcd69b232ddf26df511
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-19 11:01:31 -0800

    Configured AWS EB and adjusted server to work with AWS

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 var handlers
 
-var port = 8080;
+var port = process.env.PORT || 8888;
 
 var ip = '127.0.0.1';
 
 var server = http.createServer(require('./request-handler.js'));
 console.log('Server running on ' + ip + ':' + port);
-server.listen(port, ip);
+server.listen(port);
