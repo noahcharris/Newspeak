ea636b539c4738a3ed108eefbed2da5f9dfe5f6c
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-19 13:01:24 -0800

    testing

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,12 +1,12 @@
 
-var mysql = require('mysql');
+//var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT
-});
+// var connection = mysql.createConnection({
+//   host: process.env.RDS_HOSTNAME,
+//   user: process.env.RDS_USERNAME,
+//   password: process.env.RDS_PASSWORD,
+//   port: process.env.RDS_PORT
+// });
 
 //connection.connect();
 
