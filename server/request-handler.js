5ef08eb41244a8ea657d17019c0126bd8ecce9f9
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-19 12:55:21 -0800

    Added npm mysql

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,4 +1,16 @@
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.RDS_PORT
+});
+
+
+
+
 
 module.exports = function (request, response) {
   console.log('serving ' + request.method + ' request at ' + request.url);
