adab00d93d015d4ab95f0cb6c4cfc1efd0812d54
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-19 15:00:19 -0800

    reorganizing for tests

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  response.end('Hello world');
+
 }).listen(process.env.PORT || 8888);    
