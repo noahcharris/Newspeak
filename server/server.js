b630adb5077826e52244c6d069220588b0cb8a82
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-20 11:44:34 -0800

    Made a basic connection to postgres database

--- server/server.js
+++ server/server.js
@@ -9,6 +9,26 @@ app.get('/', function(request, response) {
   response.send('Hello world!');
 });
 
-//binds to 3000 by default
 app.listen(8080);
 console.log('express server started listening...');
+
+
+
+var pg = require('pg');
+
+var conString = 'postgres://noahharris@localhost:5432/noahharris';
+
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
+    client.end();
+  });
+});
