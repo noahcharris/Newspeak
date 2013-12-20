28ab112e40cc928191452eeba8a9970b7c82079a
Brandon Cooper <cooperjbrandon@gmail.com>
2013-12-19 16:41:52 -0800

    d3 works with angular

--- client/directive.js
+++ client/directive.js
@@ -1,28 +1,19 @@
-// angular.module('newSpeakApp')
-// .directive('barChart', function(){
-// 	var chart = d3.custom.barChart();
-// 	return {
-// 		restrict: 'E',
-// 		replace: true,
-// 		template: '<div class="chart"></div>',
-// 		scope:{
-// 			height: '=height',
-// 			data: '=data',
-// 			hovered: '&hovered'
-// 		},
-// 		link: function(scope, element, attrs) {
-// 			var chartEl = d3.select(element[0]);
-// 			chart.on('customHover', function(d, i){
-// 				scope.hovered({args:d});
-// 			});
+angular.module('newSpeakApp')
+.directive('barChart', function(){
+	var chart = d3.custom.barChart();
+	return {
+		restrict: 'E',
+		replace: true,
+		template: '<div class="chart"></div>',
+		scope:{
+			data: '=data'
+		},
+		link: function(scope, element, attrs) {
+			var chartEl = d3.select(element[0]);
 
-// 			scope.$watch('data', function (newVal, oldVal) {
-// 				chartEl.datum(newVal).call(chart);
-// 			});
-
-// 			scope.$watch('height', function(d, i){
-// 				chartEl.call(chart.height(scope.height));
-// 			})
-// 		}
-// 	}
-// });
+			scope.$watch('data', function (newVal, oldVal) {
+				chartEl.datum(newVal).call(chart);
+			});
+		}
+	}
+});
