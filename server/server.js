9a8e4dcca8f4bf54e936f4f0a4d28a7e6776dc89
Noah Harris <noah.christopher.harris@gmail.com>
2013-12-20 17:00:50 -0800

    Added some middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ var routes = require('./routes.js');
 
 
 app = express();
+app.set('port', 8080);
+
+app.use(function(request, response, next) {
+  console.log('received '+request.method+' request at '+request.url);
+  next();
+});
+
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 
 app.get('/', function(request, response) {
   response.send('Hello world!');
@@ -17,5 +25,5 @@ app.post('/data', routes.receiveData);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
-console.log('express server listening..');
+app.listen(app.get('port'));
+console.log('express server listening on port %s', app.get('port'));
