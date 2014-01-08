c349c9e43445faa6e9fe01d70018041b59c05104
TuringTesties <jackson.clowes@gmail.com>
2014-01-07 16:56:04 -0800

    Adding python data processing tools to the master

---
+++ Runfile.py
@@ -0,0 +1,14 @@
+import sys
+import nltk
+import re
+# from __future__ import division
+# from nltk.corpus import stopwords
+# from nltk.dataGenerator import dataBuilder
+# from nltk.analyzers import textProcessor, mostCommonWords, concordanceCounter, mostCommonConcordancer
+# from collections import Counter
+# import string
+import io
+import json
+
+stopWordList = stopwords.words('english')
+
